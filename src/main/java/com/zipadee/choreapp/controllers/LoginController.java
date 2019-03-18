3471ee9285a3f7cdeed2b7b8e1a177f1ff6086c1
Sarah Dixon <dixonspectives@gmail.com>
2019-03-18 16:52:23 -0500

    Tentative login page

--- src/main/java/com/zipadee/choreapp/controllers/LoginController.java
+++ src/main/java/com/zipadee/choreapp/controllers/LoginController.java
@@ -1,15 +1,35 @@
 package com.zipadee.choreapp.controllers;
 
+import com.zipadee.choreapp.form.LoginForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("login")
 public class LoginController {
 
-    @RequestMapping(value = "")
-    public String index() {
+    //getting login form page
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public String getLoginForm() {
+        return "login/index";
+    }
+    //checking that user has login credentials
+    @RequestMapping(value = "", method=RequestMethod.POST)
+    public String login(@ModelAttribute(name="loginForm") LoginForm loginForm, Model model) {
+
+        String username = loginForm.getUsername();
+        String password = loginForm.getPassword();
+
+        if("admin".equals(username) && "admin".equals(password)) {
+            // if the user is found, redirect to home page
+            return "home/home";
+        }
+        // if user or password is invalid
+        model.addAttribute("invalidCredentials", true);
         return "login/index";
 
     }
